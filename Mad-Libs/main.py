88f13b86e4de6bd791a514f8479c60a91c4f015d
[Azk|Dev] Azkron <90651411+Azk0rn@users.noreply.github.com>
2023-05-09 12:54:28 +0200

    Create main.py

---
+++ Mad-Libs/main.py
@@ -0,0 +1,9 @@
+adj = input("Adjective : ")
+verb1 = input("Verbe 1 : ")
+verb2 = input("Verbe 2 : ")
+famous_person = input("Fameuse personne : ")
+
+malib = f"La programmation informatique, c'est tellement {adj} ! Je suis toujours très enthousiaste \
+J'aime {verb1}. Restez hydraté et {verb2} comme si vous étiez {famous_person} !"
+
+print(malib)
