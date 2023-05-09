7a2d764ed798f49d07fd0b79848faa930f5ce84d
[Azk|Dev] Azkron <90651411+Azk0rn@users.noreply.github.com>
2023-05-09 13:07:45 +0200

    Create main.py

---
+++ Guess_Number/main.py
@@ -0,0 +1,31 @@
+import random
+cheat = False
+
+def guess(x):
+    random_number = random.randint(1, x)
+    if cheat: 
+        print(random_number)
+    guess = 0 
+    while guess != random_number:
+        guess = int(input(f"Deviner le nombre entre 1 et {x} ! : "))
+        if guess < random_number: 
+            print("Désoler, essaye encore. Plus haut")
+        elif guess > random_number:
+            print("Désoler, essaye encore. Plus bas")
+    print("Bravo vous avez trouvé le nombre juste !")
+
+def computer_guess(x):
+    low = 1 
+    high = x
+    feedback = ''
+
+    while feedback != 'C':
+        guess = random.randint(low, high)
+        feedback = input(f'Mon nombre est : {guess}, Ecrit (L) pour Plus Bas ,(H) pour Plus Haut , si l\'ordinateur à trouvé votre nombre (C) ! : ')
+        if feedback == 'L':
+            high = guess - 1
+        elif feedback == 'H':
+            low = guess + 1
+    print(f"L'ordinateur à trouvé votre nombre, C'était : {guess}")
+
+computer_guess(10)
